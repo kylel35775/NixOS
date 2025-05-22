53c8d605db60b86ecc9f51a44a990e6145470c8c
kylel35775 <kylel35775@gmail.com>
2025-05-22 11:24:28 -0500

    Remove home/users directory, move user creation to <user>/default.nix, which will import user files and configs

--- hosts/LenovoC940/home/default.nix
+++ hosts/LenovoC940/home/default.nix
@@ -1,5 +1,5 @@
 _: {
   imports = [
-    ./users
+    ./kylel
   ];
 }
--- hosts/LenovoC940/home/kylel/default.nix
+++ hosts/LenovoC940/home/kylel/default.nix
@@ -1,6 +1,10 @@
-_: let
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  username = builtins.baseNameOf (builtins.toString ./.);
   inherit (import ./user-variables.nix) waybarChoice;
-in {
   imports = [
     ./amfora.nix
     ./bash.nix
@@ -38,4 +42,46 @@ in {
     ./zoxide.nix
     ./zsh
   ];
+in {
+  nix.settings.allowed-users = ["${username}"];
+
+  users.users.${username} = {
+    isNormalUser = true;
+    description = "";
+    extraGroups = [
+      "adbusers"
+      "docker"
+      "input"
+      "libvirtd"
+      "lp"
+      "networkmanager"
+      "scanner"
+      "wheel"
+    ];
+
+    shell = pkgs.zsh;
+    ignoreShellProgramCheck = true;
+  };
+
+  home-manager = {
+    users.${username} = {
+      inherit imports;
+
+      home = {
+        username = "${username}";
+        homeDirectory = "/home/${username}";
+        stateVersion = "23.11";
+      };
+      xdg.userDirs = {
+        enable = true;
+        createDirectories = true;
+        extraConfig = {
+          XDG_MUSIC_LIBRARY_DIR = "/home/${username}/Music/Library";
+          XDG_MUSIC_DOWNLOADS_DIR = "/home/${username}/Music/Downloads";
+          XDG_SCREENSHOTS_DIR = "/home/${username}/Pictures/Screenshots";
+          XDG_WALLPAPERS_DIR = "/home/${username}/Pictures/Wallpapers";
+        };
+      };
+    };
+  };
 }
