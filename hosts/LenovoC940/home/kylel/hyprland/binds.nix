2da98c3a7ab59a88fb56eb6fc656ed62e17eff46
kylel35775 <kylel35775@gmail.com>
2025-05-23 11:33:57 -0500

    Added microphone mute keybind. Currently functional media keys can now bypass lock screen

--- hosts/LenovoC940/home/kylel/hyprland/binds.nix
+++ hosts/LenovoC940/home/kylel/hyprland/binds.nix
@@ -78,17 +78,28 @@ in {
       "$modifier,mouse_up,workspace, e-1"
       "ALT,Tab,cyclenext"
       "ALT,Tab,bringactivetotop"
-      ",XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+
+      # ",XF86PowerOff"
+    ];
+
+    # bind[l] - Allow keybinds to bypass lock screen
+    bindl = [
+      ",XF86AudioRaiseVolume,exec,wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
       ",XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-      " ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
       ",XF86AudioPlay, exec, playerctl play-pause"
       ",XF86AudioPause, exec, playerctl play-pause"
       ",XF86AudioNext, exec, playerctl next"
       ",XF86AudioPrev, exec, playerctl previous"
+
+      ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+
       ",XF86MonBrightnessDown,exec,brightnessctl set 5%-"
-      ",XF86MonBrightnessUp,exec,brightnessctl set +5%"
+      ",XF86MonBrightnessUp,exec,brightnessctl set 5%+"
     ];
 
+    # bind[m] - Mouse keybinds
     bindm = [
       "$modifier, mouse:272, movewindow"
       "$modifier, mouse:273, resizewindow"
