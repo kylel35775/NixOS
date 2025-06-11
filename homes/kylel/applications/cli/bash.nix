fa075edf174c131485e6836e1a1df3fbdf2c1ff7
kylel35775 <kylel35775@gmail.com>
2025-06-10 20:04:45 -0500

    Added 'fhr' and 'fhu' aliases to bash.nix.

--- homes/kylel/applications/cli/bash.nix
+++ homes/kylel/applications/cli/bash.nix
@@ -14,6 +14,8 @@
       sv = "sudo nvim";
       fr = "nh os switch --hostname ${profile}";
       fu = "nh os switch --hostname ${profile} --update";
+      fhr = "nh home switch --backup-extension backup";
+      fhu = "nh home switch --backup-extension backup --update";
       zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/raw/main/install-zaneyos.sh)";
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       v = "nvim";
